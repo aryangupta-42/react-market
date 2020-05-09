c193e6c2611c237ea26cc1ce2c8bc6ea26a72dea
Aryan <aryanguptaleo@gmail.com>
2020-05-09 05:44:22 +0530

    Added Items placeholder

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import SubHeader from '../components/layout/subHeader';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Landing = () => (
-  <div>
-    <SubHeader title="Home" />
-  </div>
-);
+import SubHeader from '../components/layout/subHeader';
+import Item from '../components/item';
+
+const useStyles = makeStyles((theme) => ({
+  itemContainer: {
+    width: '80%',
+    height: 'auto',
+    padding: theme.spacing(4),
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+}));
+
+const Landing = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <SubHeader title="Home" />
+      <div className={classes.itemContainer}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
